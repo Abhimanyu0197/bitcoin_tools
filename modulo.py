f6b9bb29bc55f542032fb4e1f60e96d750320920
AlexCurl <46902666+AlexCurl@users.noreply.github.com>
2022-10-19 14:43:42 +0300

    Update modulo.py

--- modulo.py
+++ modulo.py
@@ -1,5 +1,5 @@
 def mod(x,m): #modulo operation based on addition and subtraction x(number) m(modulus) for an arbitrarily large integers
-    if x > 0: #to check how many cut_down or add_up ops are necesssary
+    if x > 0: #to check how many cut_down or add_up ops are necessary
         if x < m:
             return x
         else:
