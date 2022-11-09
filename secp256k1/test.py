ebc50c03aa8e4816ff921d8b57690027a2d9af8a
AlexCurl <46902666+AlexCurl@users.noreply.github.com>
2022-11-09 19:38:26 +0300

    Update test.py

--- secp256k1/test.py
+++ secp256k1/test.py
@@ -1,14 +1,14 @@
-import secp256k1
+from secp256k1 import *
 
-P  = secp256k1.scalar_multiplication(2)
-print(secp256k1.point_to_upub(P))
-print(secp256k1.point_to_cpub(P))    
-G4 = secp256k1.point_multiplication(P, 2)
-G6 = secp256k1.point_addition(P, G4)
-G2 = secp256k1.point_subtraction(G6, G4)
-G2 = secp256k1.point_division(G6, 3)
-G12 = secp256k1.point_doubling(G6)
-G = secp256k1.point_halving(G2)
-print(secp256k1.on_curve(G))
-print(secp256k1.point_negation(G))
+P  = scalar_multiplication(2)
+print(point_to_upub(P))
+print(point_to_cpub(P))    
+G4 = point_multiplication(P, 2)
+G6 = point_addition(P, G4)
+G2 = point_subtraction(G6, G4)
+G2 = point_division(G6, 3)
+G12 = point_doubling(G6)
+G = point_halving(G2)
+print(on_curve(G))
+print(point_negation(G))
 
