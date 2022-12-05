86085ec36ee782bce39655434418c92978741e25
AlexCurl <46902666+AlexCurl@users.noreply.github.com>
2022-12-06 00:02:29 +0300

    Update test_gmp.c

--- ecc_gmp_C/addr_hash160_test/test_gmp.c
+++ ecc_gmp_C/addr_hash160_test/test_gmp.c
@@ -14,30 +14,24 @@
 #include "util.h"
 
 struct Elliptic_Curve {
-    
     mpz_t a;
     mpz_t b;
-	mpz_t p;
+    mpz_t p;
     mpz_t n;
-    
 };
 
 struct Point {
-    
-	mpz_t x;
-	mpz_t y;
-    
+    mpz_t x;
+    mpz_t y;
 };
 
 struct Elliptic_Curve EC; 
 struct Point Curve_G;
 
 void Point_Doubling(struct Point P, struct Point *R) {
+    mpz_t slope, temp;
+    mpz_init(temp); mpz_init(slope);
     
-	mpz_t slope, temp;
-	mpz_init(temp);
-	mpz_init(slope);
-    	
     mpz_mul_ui(temp, P.y, 2);
     mpz_invert(temp, temp, EC.p);
     mpz_mul(slope, P.x, P.x);
@@ -56,174 +50,146 @@ void Point_Doubling(struct Point P, struct Point *R) {
     mpz_sub(R->y, R->y, P.y);
     mpz_mod(R->y, R->y, EC.p);
     
-	mpz_clear(temp);
-	mpz_clear(slope);
+    mpz_clear(temp); mpz_clear(slope);
 }
 
 void Point_Addition(struct Point P, struct Point Q, struct Point *R) {
-    
     struct Point S;
-    mpz_init(S.x); mpz_init(S.y);        
+    mpz_init(S.x); mpz_init(S.y);
     mpz_set(S.x, P.x); mpz_set(S.y, P.y);
-        
-	if(mpz_cmp_ui(S.x, 0) == 0 && mpz_cmp_ui(S.y, 0) == 0) {
-		mpz_set(R->x, Q.x);
-		mpz_set(R->y, Q.y);
-		return;
-	}
     
-	if(mpz_cmp_ui(Q.x, 0) == 0 && mpz_cmp_ui(Q.y, 0) == 0) {
-		mpz_set(R->x, S.x);
-		mpz_set(R->y, S.y);
-		return;
-	}
+    if(mpz_cmp_ui(S.x, 0) == 0 && mpz_cmp_ui(S.y, 0) == 0) {
+        mpz_set(R->x, Q.x);
+        mpz_set(R->y, Q.y);
+        return;
+    }
+    if(mpz_cmp_ui(Q.x, 0) == 0 && mpz_cmp_ui(Q.y, 0) == 0) {
+        mpz_set(R->x, S.x);
+        mpz_set(R->y, S.y);
+        return;
+    }
     
-	mpz_t temp;
-	mpz_init(temp);
+    mpz_t temp; mpz_init(temp);
     
-	if(mpz_cmp_ui(Q.y, 0) != 0) { 
-		mpz_sub(temp, EC.p, Q.y);
-		mpz_mod(temp, temp, EC.p);
-	} else
-		mpz_set_ui(temp, 0);
+    if(mpz_cmp_ui(Q.y, 0) != 0) {
+        mpz_sub(temp, EC.p, Q.y);
+        mpz_mod(temp, temp, EC.p);
+    } else {
+        mpz_set_ui(temp, 0);
+    }
+    
+    if(mpz_cmp(S.y, temp) == 0 && mpz_cmp(S.x, Q.x) == 0) {
+        mpz_set_ui(R->x, 0);
+        mpz_set_ui(R->y, 0);
+        mpz_clear(temp);
+        return;
+    }
+    if(mpz_cmp(S.x, Q.x) == 0 && mpz_cmp(S.y, Q.y) == 0) {
+        Point_Doubling(S, R);
+        mpz_clear(temp);
+        return;
+    } else {
+        mpz_t slope;
+        mpz_init_set_ui(slope, 0);
         
-	if(mpz_cmp(S.y, temp) == 0 && mpz_cmp(S.x, Q.x) == 0) {
-		mpz_set_ui(R->x, 0);
-		mpz_set_ui(R->y, 0);
-		mpz_clear(temp);
-		return;
-	}
-	
-	if(mpz_cmp(S.x, Q.x) == 0 && mpz_cmp(S.y, Q.y) == 0)	{
-		Point_Doubling(S, R);		
-		mpz_clear(temp);
-		return;		
-	} else {
-		mpz_t slope;
-		mpz_init_set_ui(slope, 0);
+        mpz_sub(temp, S.x, Q.x);
+        mpz_mod(temp, temp, EC.p);
         
-		mpz_sub(temp, S.x, Q.x);
-		mpz_mod(temp, temp, EC.p);
+        mpz_invert(temp, temp, EC.p);
+        mpz_sub(slope, S.y, Q.y);
+        mpz_mul(slope, slope, temp);
+        mpz_mod(slope, slope, EC.p);
         
-		mpz_invert(temp, temp, EC.p);
-		mpz_sub(slope, S.y, Q.y);
-		mpz_mul(slope, slope, temp);
-		mpz_mod(slope, slope, EC.p);
+        mpz_mul(R->x, slope, slope);
+        mpz_sub(R->x, R->x, S.x);
+        mpz_sub(R->x, R->x, Q.x);
+        mpz_mod(R->x, R->x, EC.p);
         
-		mpz_mul(R->x, slope, slope);
-		mpz_sub(R->x, R->x, S.x);
-		mpz_sub(R->x, R->x, Q.x);
-		mpz_mod(R->x, R->x, EC.p);
+        mpz_sub(temp, S.x, R->x);
+        mpz_mul(R->y, slope, temp);
+        mpz_sub(R->y, R->y, S.y);
+        mpz_mod(R->y, R->y, EC.p);
         
-		mpz_sub(temp, S.x, R->x);
-		mpz_mul(R->y, slope, temp);
-		mpz_sub(R->y, R->y, S.y);
-		mpz_mod(R->y, R->y, EC.p);
-        		
-		mpz_clear(temp);
-		mpz_clear(slope);
-        mpz_clear(S.x); mpz_clear(S.y);
-		return;
-	}
+        mpz_clear(temp);
+        mpz_clear(slope);
+        mpz_clear(S.x);
+        mpz_clear(S.y);
+        return;
+    }
 }
 
 void Scalar_Multiplication(struct Point *R, mpz_t m) {
-    
     struct Point P;
     mpz_init(P.x); mpz_init(P.y);
     mpz_set(P.x, Curve_G.x); mpz_set(P.y, Curve_G.y);
-	struct Point Q, T;
-	mpz_init(Q.x); mpz_init(Q.y);
-	mpz_init(T.x); mpz_init(T.y);
-	long no_of_bits, loop;	
-	no_of_bits = mpz_sizeinbase(m, 2);
-	mpz_set_ui(R->x, 0);
-	mpz_set_ui(R->y, 0);
-	if(mpz_cmp_ui(m, 0) == 0)
-		return;
-        		
-	mpz_set(Q.x, P.x);
-	mpz_set(Q.y, P.y);
+    struct Point Q, T;
+    mpz_init(Q.x); mpz_init(Q.y); mpz_init(T.x); mpz_init(T.y);
+    long no_of_bits, loop;
+    no_of_bits = mpz_sizeinbase(m, 2);
+    mpz_set_ui(R->x, 0); mpz_set_ui(R->y, 0);
+    if(mpz_cmp_ui(m, 0) == 0) return;
     
-	if(mpz_tstbit(m, 0) == 1){
-		mpz_set(R->x, P.x);
-		mpz_set(R->y, P.y);
-	}
-
-	for(loop = 1; loop < no_of_bits; loop++) {
-		mpz_set_ui(T.x, 0);
-		mpz_set_ui(T.y, 0);
-		Point_Doubling(Q, &T);
-		mpz_set(Q.x, T.x);
-		mpz_set(Q.y, T.y);
-		mpz_set(T.x, R->x);
-		mpz_set(T.y, R->y);
-		if(mpz_tstbit(m, loop))
-			Point_Addition(T, Q, R);
+    mpz_set(Q.x, P.x); mpz_set(Q.y, P.y);
+    
+    if(mpz_tstbit(m, 0) == 1) {
+        mpz_set(R->x, P.x);
+        mpz_set(R->y, P.y);
+    }
+    
+    for(loop = 1; loop < no_of_bits; loop++) {
+        mpz_set_ui(T.x, 0); mpz_set_ui(T.y, 0);
+        Point_Doubling(Q, &T);
+        mpz_set(Q.x, T.x); mpz_set(Q.y, T.y);
+        mpz_set(T.x, R->x); mpz_set(T.y, R->y);
+        if(mpz_tstbit(m, loop)) Point_Addition(T, Q, R);
 	}
     
     mpz_clear(P.x); mpz_clear(P.y);
-	mpz_clear(Q.x); mpz_clear(Q.y);
-	mpz_clear(T.x); mpz_clear(T.y);    
+    mpz_clear(Q.x); mpz_clear(Q.y);
+    mpz_clear(T.x); mpz_clear(T.y);    
 }
 
 void Point_Multiplication(struct Point P, struct Point *R, mpz_t m) {
+    struct Point Q, T;
+    mpz_init(Q.x); mpz_init(Q.y); mpz_init(T.x); mpz_init(T.y);
+    long no_of_bits, loop;
+    no_of_bits = mpz_sizeinbase(m, 2);
+    mpz_set_ui(R->x, 0); mpz_set_ui(R->y, 0);
+    if(mpz_cmp_ui(m, 0) == 0) return;
     
-	struct Point Q, T;
-	mpz_init(Q.x); mpz_init(Q.y);
-	mpz_init(T.x); mpz_init(T.y);
-	long no_of_bits, loop;	
-	no_of_bits = mpz_sizeinbase(m, 2);
-	mpz_set_ui(R->x, 0);
-	mpz_set_ui(R->y, 0);
+    mpz_set(Q.x, P.x); mpz_set(Q.y, P.y);
     
-	if(mpz_cmp_ui(m, 0) == 0)
-		return;
-		
-	mpz_set(Q.x, P.x);
-	mpz_set(Q.y, P.y);
+    if(mpz_tstbit(m, 0) == 1) {
+        mpz_set(R->x, P.x);
+        mpz_set(R->y, P.y);
+    }
     
-	if(mpz_tstbit(m, 0) == 1){
-		mpz_set(R->x, P.x);
-		mpz_set(R->y, P.y);
-	}
-
-	for(loop = 1; loop < no_of_bits; loop++) {
-		mpz_set_ui(T.x, 0);
-		mpz_set_ui(T.y, 0);
-		Point_Doubling(Q, &T);
-		mpz_set(Q.x, T.x);
-		mpz_set(Q.y, T.y);
-		mpz_set(T.x, R->x);
-		mpz_set(T.y, R->y);
-		if(mpz_tstbit(m, loop))
-			Point_Addition(T, Q, R);
-	}
-
-	mpz_clear(Q.x); mpz_clear(Q.y);
-	mpz_clear(T.x); mpz_clear(T.y);
+    for(loop = 1; loop < no_of_bits; loop++) {
+        mpz_set_ui(T.x, 0); mpz_set_ui(T.y, 0);
+        Point_Doubling(Q, &T);
+        mpz_set(Q.x, T.x); mpz_set(Q.y, T.y);
+        mpz_set(T.x, R->x); mpz_set(T.y, R->y);
+		if(mpz_tstbit(m, loop)) Point_Addition(T, Q, R);
+    }
+    
+    mpz_clear(Q.x); mpz_clear(Q.y);
+    mpz_clear(T.x); mpz_clear(T.y);
 }
 
 void Point_Negation(struct Point *A) {
-    
-	mpz_sub(A->y, EC.p, A->y);
-    
+    mpz_sub(A->y, EC.p, A->y);
 }
 
 void Point_Subtraction(struct Point A, struct Point *B, struct Point *R) {
-    
     if (mpz_cmp(A.x, B->x) == 0 && mpz_cmp(A.y, B->y) == 0) {
         mpz_set_ui(R->x, 0);
         mpz_set_ui(R->y, 0);
         return;
-    }
-    else {
+    } else {
         struct Point Q;
         Point_Negation(B);
-        mpz_init(Q.x);
-        mpz_init(Q.y);        
-        mpz_set(Q.x, B->x);
-        mpz_set(Q.y, B->y);
+        mpz_init(Q.x); mpz_init(Q.y);
+        mpz_set(Q.x, B->x); mpz_set(Q.y, B->y);
         Point_Addition(A, Q, R);
         mpz_clear(Q.x); mpz_clear(Q.y);
         return;      
@@ -231,16 +197,13 @@ void Point_Subtraction(struct Point A, struct Point *B, struct Point *R) {
 }
 
 void Point_Division(struct Point D, struct Point *S, mpz_t k) {
-    
-    mpz_t m;
-    mpz_init(m);
+    mpz_t m; mpz_init(m);
     mpz_invert(m, k, EC.n);
     Point_Multiplication(D, S, m);
     mpz_clear(m);
 }
 
 bool Point_On_Curve(struct Point A) {
-    
     mpz_t X, Y;
     mpz_init(X); mpz_init(Y);
     mpz_pow_ui(X, A.x, 3);
@@ -249,15 +212,12 @@ bool Point_On_Curve(struct Point A) {
     mpz_pow_ui(Y, A.y, 2);
     mpz_mod(Y, Y, EC.p);
     if (mpz_cmp(X, Y) == 0) {
-        mpz_clear(X);
-        mpz_clear(Y);
-        return true; 
+        mpz_clear(X); mpz_clear(Y);
+        return true;
     } else {
-        mpz_clear(X);
-        mpz_clear(Y);
-        return false; 
+        mpz_clear(X); mpz_clear(Y);
+        return false;
     }
-    
 }
 
 static char upub[132];
@@ -271,78 +231,64 @@ static char bin_sha256[32];
 static char bin_digest[60];
 
 const char * Point_To_Upub(struct Point A) {
-    
     gmp_snprintf(upub, 132, "04%0.64Zx%0.64Zx", A.x, A.y);
     return upub;
-    
 }
 
 const char * Point_To_Cpub(struct Point A) {
-    
-    if(mpz_tstbit(A.y, 0) == 0) { gmp_snprintf(cpub, 67, "02%0.64Zx", A.x); }
-    else { gmp_snprintf(cpub, 67,"03%0.64Zx", A.x); }
+    if(mpz_tstbit(A.y, 0) == 0) { 
+        gmp_snprintf(cpub, 67, "02%0.64Zx", A.x); 
+    } else { 
+        gmp_snprintf(cpub, 67, "03%0.64Zx", A.x);
+    }
     return cpub;
-    
 }
 
 const char * Point_To_Hash160(struct Point pubkey, bool compressed) {
-    
-	char bin_rmd160[20];
-    
-	if(compressed) {
-		if(mpz_tstbit(pubkey.y, 0) == 0) {
-			gmp_snprintf (cpub, 67, "02%0.64Zx", pubkey.x);
+    char bin_rmd160[20];
+    if(compressed) {
+        if(mpz_tstbit(pubkey.y, 0) == 0) {
+            gmp_snprintf (cpub, 67, "02%0.64Zx", pubkey.x);
+        } else {
+            gmp_snprintf(cpub, 67, "03%0.64Zx", pubkey.x);
 		}
-		else	{
-			gmp_snprintf(cpub, 67, "03%0.64Zx", pubkey.x);
-		}
-		hexs2bin(cpub, bin_publickey);
-		sha256(bin_publickey, 33, bin_sha256);
+        hexs2bin(cpub, bin_publickey);
+        sha256(bin_publickey, 33, bin_sha256);
 	}
-	else	{
-		gmp_snprintf(upub, 132, "04%0.64Zx%0.64Zx", pubkey.x, pubkey.y);
-		hexs2bin(upub, bin_publickey);
-		sha256(bin_publickey, 65, bin_sha256);
+    else {
+        gmp_snprintf(upub, 132, "04%0.64Zx%0.64Zx", pubkey.x, pubkey.y);
+        hexs2bin(upub, bin_publickey);
+        sha256(bin_publickey, 65, bin_sha256);
 	}
-    
-	RMD160Data((const unsigned char*)bin_sha256, 32, bin_rmd160);
-	tohex_dst(bin_rmd160, 20, hash160);
-    return hash160;
-    
+    RMD160Data((const unsigned char*)bin_sha256, 32, bin_rmd160);
+    tohex_dst(bin_rmd160, 20, hash160);
+    return hash160;    
 }
 
 const char * Point_To_Legacy_Address(struct Point pubkey, bool compressed) {
-    
-	size_t pubaddress_size = 50;
-    
-	if(compressed) {
-		if(mpz_tstbit(pubkey.y, 0) == 0) {
-			gmp_snprintf(cpub, 68, "02%0.64Zx", pubkey.x);
-		}
-		else {
-			gmp_snprintf(cpub, 68, "03%0.64Zx", pubkey.x);
-		}
-		hexs2bin(cpub, bin_publickey);
-		sha256(bin_publickey, 33, bin_sha256);
+    size_t pubaddress_size = 50;
+    if(compressed) {
+        if(mpz_tstbit(pubkey.y, 0) == 0) {
+            gmp_snprintf(cpub, 68, "02%0.64Zx", pubkey.x);
+		} else {
+            gmp_snprintf(cpub, 68, "03%0.64Zx", pubkey.x);
+        }
+        hexs2bin(cpub, bin_publickey);
+        sha256(bin_publickey, 33, bin_sha256);
+	} else {
+        gmp_snprintf(upub, 132, "04%0.64Zx%0.64Zx", pubkey.x, pubkey.y);
+        hexs2bin(upub, bin_publickey);
+        sha256(bin_publickey, 65, bin_sha256);
 	}
-	else {
-		gmp_snprintf(upub, 132, "04%0.64Zx%0.64Zx", pubkey.x, pubkey.y);
-		hexs2bin(upub, bin_publickey);
-		sha256(bin_publickey, 65, bin_sha256);
-	}
-    
     RMD160Data(bin_sha256, 32, bin_digest + 1);
-	bin_digest[0] = 0;		
-	sha256(bin_digest, 21, bin_digest + 21);
-	sha256(bin_digest + 21, 32, bin_digest + 21);
-    
+    bin_digest[0] = 0;
+    sha256(bin_digest, 21, bin_digest + 21);
+    sha256(bin_digest + 21, 32, bin_digest + 21);
     b58enc(address, &pubaddress_size, bin_digest, 25);
     return address;
-    
 }
 
 const char * Point_To_P2SH_Address(struct Point pubkey) {
-    
     size_t pubaddress_size = 50;
     if(mpz_tstbit(pubkey.y, 0) == 0) {
         gmp_snprintf(cpub, 68, "02%0.64Zx", pubkey.x);
@@ -350,68 +296,53 @@ const char * Point_To_P2SH_Address(struct Point pubkey) {
     else {
         gmp_snprintf(cpub, 68, "03%0.64Zx", pubkey.x);
     }
-
-    hexs2bin(cpub, bin_publickey);    
+    hexs2bin(cpub, bin_publickey);
     sha256(bin_publickey, 33, bin_sha256);
-    
     RMD160Data(bin_sha256, 32, bin_digest + 2);
-	bin_digest[0] = 0x00; bin_digest[1] = 0x14;
-    
+    bin_digest[0] = 0x00; bin_digest[1] = 0x14;
     sha256(bin_digest, 22, bin_sha256);
-    
     RMD160Data(bin_sha256, 32, bin_digest + 1);
-	bin_digest[0] = 0x05;
-    
+    bin_digest[0] = 0x05;
     sha256(bin_digest, 21, bin_sha256);
-    
     sha256(bin_sha256, 32, bin_sha256);
     bin_digest[21] = bin_sha256[0];
     bin_digest[22] = bin_sha256[1];
     bin_digest[23] = bin_sha256[2];
     bin_digest[24] = bin_sha256[3];
-    
     b58enc(address, &pubaddress_size, bin_digest, 25);
     return address;
-    
 }
 
 const char * Point_To_Bech32_P2WPKH_Address(struct Point pubkey) {
-        
     if(mpz_tstbit(pubkey.y, 0) == 0) {
         gmp_snprintf(cpub, 68, "02%0.64Zx", pubkey.x);
     }
     else {
         gmp_snprintf(cpub, 68, "03%0.64Zx", pubkey.x);
     }
-
     hexs2bin(cpub, bin_publickey);    
     sha256(bin_publickey, 33, bin_sha256);
     RMD160Data(bin_sha256, 32, bin_digest);
     segwit_addr_encode(bech32_output, "bc", 0, bin_digest, 20);
     return bech32_output;
-    
 }
 
 const char * Point_To_Bech32_P2WSH_Address(struct Point pubkey) {
-    
     if(mpz_tstbit(pubkey.y, 0) == 0) {
         gmp_snprintf(cpub, 68, "02%0.64Zx", pubkey.x);
     }
     else {
         gmp_snprintf(cpub, 68, "03%0.64Zx", pubkey.x);
     }
-
     hexs2bin(cpub, bin_publickey);
     sha256(bin_publickey, 33, bin_sha256);
     segwit_addr_encode(bech32_output, "bc", 0, bin_sha256, 32);
     return bech32_output;
-    
 }
 
 char tap_tweak[] = "TapTweak";
 
 const char * Tagged_Hash(mpz_t x) {
-    
     char hex_mpz[66];
     char pub_bin[33];
     char taghash[33];
@@ -424,18 +355,15 @@ const char * Tagged_Hash(mpz_t x) {
     memmove(taghash_final + 64, pub_bin, sizeof(pub_bin));
     sha256(taghash_final, 96, bin_sha256);
     return tohex(bin_sha256, 32);
-
 }
 
 const char * Taproot_Tweak_PrivKey(mpz_t k) {
-    
     mpz_t seckey, t, s;
     mpz_init(seckey); mpz_init(t); mpz_init(s);
     struct Point Q;
     mpz_init(Q.x);
     mpz_init(Q.y);
     Scalar_Multiplication(&Q, k);
-    
     if(mpz_tstbit(Q.y, 0) == 0) {
         mpz_set(seckey, k);
     }
@@ -443,7 +371,6 @@ const char * Taproot_Tweak_PrivKey(mpz_t k) {
         mpz_sub(s, EC.n, k);
         mpz_set(seckey, s);
     }
-
     mpz_set_str(t, Tagged_Hash(Q.x), 16);
     mpz_add(seckey, seckey, t);
     mpz_mod(seckey, seckey, EC.n);
@@ -451,31 +378,26 @@ const char * Taproot_Tweak_PrivKey(mpz_t k) {
     mpz_clear(t); mpz_clear(s); mpz_clear(seckey);
     mpz_clear(Q.x); mpz_clear(Q.y);
     return number_str;
-    
 }
 
 void Public_Key_X_Coordinate_To_Taproot_Tweaked_Pubkey() {// to be continued not complete
 }
 
 const char * Point_To_Bech32m_P2TR_Address() { // to be continued not complete
-    
     char pub[] = "da4710964f7852695de2da025290e24af6d8c281de5a0b902b7135fd9fd74d21";
     gmp_snprintf(cpub, 68, "%s", pub);
     hexs2bin(cpub, bin_publickey);
     segwit_addr_encode(bech32_output, "bc", 1, bin_publickey, 32);
     return bech32_output;
-    
 }
 
 const char * Private_Key_To_WIF(mpz_t pk, bool compressed) {
-    
     size_t wif_size = 54;
     char privatekey[66];
     char bin_privatekey[39];
     bin_privatekey[0] = 0x80;
     gmp_snprintf(privatekey, 65, "%0.64Zx", pk);
     hexs2bin(privatekey, bin_privatekey+1);
-    
     if (compressed) {
         bin_privatekey[33] = 0x01;
         sha256(bin_privatekey, 34, bin_sha256);
@@ -497,7 +419,6 @@ const char * Private_Key_To_WIF(mpz_t pk, bool compressed) {
         b58enc(wif, &wif_size, bin_privatekey, 37);
         return wif;
     }
-
 }
 
 int main(int argc, char *argv[])
